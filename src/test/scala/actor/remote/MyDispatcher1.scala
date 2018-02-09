6808175300282d6e0f8e9bb8c0cf84a6e02e2fa6
Ron.Coleman <ronncoleman@gmail.com>
2018-02-09 16:29:17 -0500

    Fixed test code.

--- src/test/scala/actor/remote/MyDispatcher1.scala
+++ src/test/scala/actor/remote/MyDispatcher1.scala
@@ -1,6 +1,6 @@
 package actor.remote
 
-import parascale.actor.last.Dispatcher
+import parascale.actor.last.{Dispatcher, Task}
 
 object MyDispatcher1 extends App {
   val workers = List("localhost:8000", "localhost:9000")
@@ -13,8 +13,16 @@ class MyDispatcher1(workers: List[String]) extends Dispatcher(workers) {
   println(workers)
   def act: Unit = {
 //    Thread.sleep(250)
+    println("worker len = "+workers.length)
     (0 until workers.length).foreach { k =>
-      relays(k) ! "to worker(" + k + ")hello from Fred"
+      println("sending message to worker " + k)
+      relays(k) ! "to worker(" + k + ") hello from dispatcher"
     }
+
+    while (true)
+      receive match {
+        case task: Task if task.kind == Task.REPLY =>
+          println("received reply " + task)
+      }
   }
 }
